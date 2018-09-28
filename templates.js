ca57277da479f6c833aa628f5fea45fa11c27498
wangtianhua <752990270@qq.com>
2018-09-28 12:21:24 +0800

    update

--- templates.js
+++ templates.js
@@ -4,6 +4,26 @@ module.exports = {
       "name": "project",
       "path": "divawth/bell-project",
       "desc": "new a project"
+    },
+    {
+      "name": "bell-ui",
+      "path": "divawth/bell-ui",
+      "desc": "bell-ui source"
+    },
+    {
+      "name": "bell-ui",
+      "path": "divawth/bell-docsify",
+      "desc": "bell-docsify"
+    },
+    {
+      "name": "bell",
+      "path": "divawth/bell",
+      "desc": "bell function"
+    },
+    {
+      "name": "cropper",
+      "path": "divawth/cropper",
+      "desc": "cropper"
     }
   ]
 };
